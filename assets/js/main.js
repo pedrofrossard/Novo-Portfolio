4f4babaa0261d97ffaa0b65a5dd2e2550e02ae0f
pedrofrossard <pedrohenriquefrossard@gmail.com>
2023-04-11 14:21:09 -0300

    Alteração

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ function updateProfessionalExperience(profileData) {
     professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
         return `
             <li>
-                <h3> class="title">${experience.name}</h3>
+                <h3 class="title">${experience.name}</h3>
                 <p class="period">${experience.period}</p>
                 <p>${experience.description}</p>
             </li>
